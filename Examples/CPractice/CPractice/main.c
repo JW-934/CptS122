c5ac68c94c4e049cf059ce02e6570b492ae2c97d
Jesse <jessew7980@gmail.com>
2022-01-14 11:01:16 -0800

    Update

--- Examples/CPractice/CPractice/main.c
+++ Examples/CPractice/CPractice/main.c
@@ -1,5 +1,6 @@
 /*
 Programmer: Jesse Watson
+Date: January 14, 2022
 */
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -7,9 +8,35 @@ Programmer: Jesse Watson
 #include <stdio.h>
 #include <string.h>
 
+typedef struct student // name on this line optional
+{
+	char lastname[25];
+	char firstname[25];
+	int id;
+	double gpa;
+}Student;
+
 int main(void)
 {
+	FILE* infile = fopen("data.csv", "r");
+	char line[100] = ""; // empty string line[0] = '\0'; with one array fgets will overwrite each time, array of structs better
+	Student studentData[100] = { {"", "", 0, 0.0} };
+	int lineCount = 0;
 
+	if (infile != NULL) // NULL pointer returned if fails to open
+	{
+		// File opened successfully
+		// 1. read in header line
+		fgets(line, 100, infile); // reads in one less than max count to leave room for '\0'
+		puts(line);
 
+		// can't read anymore lines of data; fgets() returns NULL pointer
+		while (fgets(line, 100, infile) != NULL) // !feof(infile) fgets returns max count unless there is no data left
+		{
+			puts(line);
+			// strtok () to parse a line; .csv file
+		}
+		fclose(infile);
+	}
 	return 0;
 }
