adc52801796ba78bfe4dd9e3fdc26bd4b371b2dc
Jesse <jessew7980@gmail.com>
2022-01-18 14:28:47 -0800

    Update PA1

--- Programming Assignments/PA1 (Fitbit Data)/PA1 (Fitbit Data)/Main.c
+++ Programming Assignments/PA1 (Fitbit Data)/PA1 (Fitbit Data)/Main.c
@@ -10,7 +10,18 @@
 
 int main(void)
 {
+	FILE* infile = fopen("FitbitData.csv", "r");
 
+	if (infile != NULL) // Input file successfully opened
+	{
+		FILE* outfile = fopen("Results.csv", "w");
+		if (outfile != NULL) // Output file successfully opened
+		{
 
+		
+		
+		
+		}
+	}
 	return 0;
 }
--- Programming Assignments/PA1 (Fitbit Data)/PA1 (Fitbit Data)/PA1.h
+++ Programming Assignments/PA1 (Fitbit Data)/PA1 (Fitbit Data)/PA1.h
@@ -12,5 +12,6 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
+#include <string.h>
 
 #endif
