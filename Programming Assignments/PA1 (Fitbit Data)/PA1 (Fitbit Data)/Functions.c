514c69562733cac86d54c9207aca0cd41a8be2ac
Jesse <jessew7980@gmail.com>
2022-01-19 10:25:58 -0800

    PA1 Update

--- Programming Assignments/PA1 (Fitbit Data)/PA1 (Fitbit Data)/Functions.c
+++ Programming Assignments/PA1 (Fitbit Data)/PA1 (Fitbit Data)/Functions.c
@@ -2,7 +2,7 @@
 * Programmer: Jesse Watson
 * Class: CptS 122, Spring 2022; Lab Section 6
 * Programming Assignment: PA1
-* Date: January 18, 2022
+* Date: January 18, 2022, January 19, 2022
 * Description: This program analyzes Fitbit data
 */
 
