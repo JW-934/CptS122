1a13fc5ef712d6fb2adeedf13ead848af2b4df1d
Jesse <jessew7980@gmail.com>
2022-01-27 09:17:38 -0800

    Create PA2

---
+++ Programming Assignments/PA2 (Music Manager)/PA2.c
@@ -0,0 +1,9 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 122, Spring 2022; Lab Section 6
+* Assignment: PA2
+* Date: January 27, 2022
+* Description:
+*/
+
+#include "PA2.h"
---
+++ Programming Assignments/PA2 (Music Manager)/PA2.h
@@ -0,0 +1,18 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 122, Spring 2022; Lab Section 6
+* Assignment: PA2
+* Date: January 27, 2022
+* Description:
+*/
+
+#ifndef PA2_H
+
+#define PA2_H
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#endif
