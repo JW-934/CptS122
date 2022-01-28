97e17e2a7e222fb6a787b7ab69d0cf87274d6994
Jesse <jessew7980@gmail.com>
2022-01-28 10:17:28 -0800

    Update PA2

--- Programming Assignments/PA2 (Music Manager)/PA2.c
+++ Programming Assignments/PA2 (Music Manager)/PA2.c
@@ -2,7 +2,7 @@
 * Programmer: Jesse Watson
 * Class: CptS 122, Spring 2022; Lab Section 6
 * Assignment: PA2
-* Date: January 27, 2022
+* Date: January 27, 2022, January 28, 2022
 * Description:
 */
 
--- Programming Assignments/PA2 (Music Manager)/PA2.h
+++ Programming Assignments/PA2 (Music Manager)/PA2.h
@@ -2,7 +2,7 @@
 * Programmer: Jesse Watson
 * Class: CptS 122, Spring 2022; Lab Section 6
 * Assignment: PA2
-* Date: January 27, 2022
+* Date: January 27, 2022, January 28, 2022
 * Description:
 */
 
@@ -14,5 +14,18 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+typedef struct duration
+{
+	int minutes;
+	int seconds;
+}Duration;
+
+typedef struct record
+{
+	char name[20];
+	Duration time;
+}Record;
+
 
 #endif
