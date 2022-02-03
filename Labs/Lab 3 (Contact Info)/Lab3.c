67618e357ab218935aeab0e0a963652ea265371b
Jesse <jessew7980@gmail.com>
2022-02-02 16:14:47 -0800

    Update Lab 3

--- Labs/Lab 3 (Contact Info)/Lab3.c
+++ Labs/Lab 3 (Contact Info)/Lab3.c
@@ -7,3 +7,143 @@
 */
 
 #include "Lab3.h"
+
+// Description: Allocates space for a Node on the heap and initializes the Node with the information found in newData.
+// Returns: The address of the start of the block of memory on the heap or NULL if no memory was allocated
+Node* makeNode(Contact newData)
+{
+	Node* newNode = (Node *)malloc(sizeof(Node));
+
+	if (newNode != NULL) // node memory allocated
+	{
+		newNode->data = newData;
+		newNode->pNext = NULL;
+		newNode->pPrev = NULL;
+	}
+	return newNode;
+}
+
+// Description: Uses makeNode () to allocate space for a new Node and inserts the new Node into the front of the list.
+// Returns: TRUE if memory was allocated for a Node; FALSE otherwise
+Boolean insertContactAtFront(Node** pList, Contact newData)
+{
+
+}
+
+// Description: Uses makeNode () to allocate space for a new Node and inserts the new Node into the list in
+//              alphabetic order ('a' - 'z') based on the name field
+// Returns: TRUE if memory was  allocated for a Node; FALSE otherwise
+Boolean insertContactInOrder(Node** pList, Contact newData)
+{
+	Node* newNode = makeNode(newData), *pCur = *pList, *pPrev = NULL;
+	Boolean success = FALSE;
+
+	if (newNode != NULL)
+	{
+		while (pCur != NULL && strcmp(pCur->data.name, newNode->data.name) < 0)
+		{
+			pPrev = pCur;
+			pCur = pCur->pNext;
+		}
+		newNode->pNext = pCur;
+		if (pPrev != NULL)
+		{
+			pPrev->pNext = newNode;
+		}
+		else
+		{
+			*pList = newNode;
+		}
+		success = TRUE;
+	}
+	return success;
+}
+
+// Description: Deletes a Contact in the list based on the name field; deletes the first occurrence of the name
+// Returns: TRUE if the Contact was found; FALSE otherwise
+Boolean deleteContact(Node** pList, Contact searchContact)
+{
+
+}
+
+// Description: Edits a Contact in the list based on the name field; edits the first occurrence of the name
+// Returns: TRUE if the Contact was found; FALSE otherwise
+Boolean editContact(Node* pList, Contact searchContact)
+{
+
+}
+
+// Description: Loads all Contact information from the given file, in alphabetic order, based on the name, into the list
+// Returns: TRUE if all Contacts were loaded; FALSE otherwise
+Boolean loadContacts(FILE* infile, Node** pList)
+{
+	char line[100] = "";
+	Boolean success = FALSE;
+	
+	Contact newContact;
+
+	while (fgets(line, 100, infile) != NULL)
+	{
+		////while (fgets(line, 100, infile))
+		//{
+		strcpy(newContact.name, strtok(line, ","));
+		strcpy(newContact.phone, strtok(NULL, ","));
+		strcpy(newContact.email, strtok(NULL, ","));
+		strcpy(newContact.title, strtok(NULL, "."));
+		strcat(newContact.title, ".");
+
+		success = insertContactInOrder(pList, newContact);
+	
+		line[0] = '\0';
+	}
+		//success = insertContactInOrder(&pList, *newContact);
+	//}
+	return success;
+}
+
+// Description: Stores all Contact information from the list into the given file
+// Returns: TRUE if all Contacts were stored; FALSE otherwise
+Boolean storeContacts(FILE* infile, Node* pList) 
+{
+
+}
+
+int getOption()
+{
+	int option = 0;
+	printMenu();
+	do
+	{
+		printf("Make your selection > ");
+		scanf("%d", &option);
+	} while (option < 1 || option > 7 || option == 0);
+	return option;
+}
+
+// Description: Prints all contact information in the list
+// Returns: Nothing
+void printList(Node* pList)
+{
+	if (pList != NULL)
+	{
+		printf("--> %s, %s, %s, %s\n", pList->data.name, pList->data.phone, pList->data.email, pList->data.title);
+		printList(pList->pNext);
+	}
+	else // base case
+	{
+		//printf(" -->\n");
+		printf("\n");
+	}
+}
+
+void printMenu()
+{
+	printf("********** Contact Information **********\n\n");
+	printf("1. Add Contact\n");
+	printf("2. Remove Contact\n");
+	printf("3. Edit Contact\n");
+	printf("4. Print Contact\n");
+	printf("5. Store Contact\n");
+	printf("6. Load Contact\n");
+	printf("7. Exit\n\n");
+}
--- Labs/Lab 3 (Contact Info)/main.c
+++ Labs/Lab 3 (Contact Info)/main.c
@@ -10,7 +10,50 @@
 
 int main(void)
 {
+	int option = 0;
+	Node* pHead = NULL;
+	FILE* infile = NULL;
+	Boolean loadSuccess = FALSE;
 
+	do
+	{
+		option = getOption();
 
+		switch (option)
+		{
+		case 1: // add
+			break;
+		case 2: // remove
+			break;
+		case 3: // edit
+			break;
+		case 4: // print
+			printList(pHead);
+			break;
+		case 5: // store
+			break;
+		case 6: // load
+			system("cls");
+			
+			infile = fopen(INPUTFILE, "r");
+
+			if (infile != NULL)
+			{
+				loadSuccess = loadContacts(infile, &pHead);
+				if (loadSuccess == TRUE)
+				{
+					printf("\nContacts loaded!\n\n");
+				}
+				fclose(infile);
+			}
+			else
+			{
+				printf("Could not access contact file!\n");
+			}
+			break;
+		case 7: // exit
+			break;
+		}
+	} while (option != 7);
 	return 0;
 }
