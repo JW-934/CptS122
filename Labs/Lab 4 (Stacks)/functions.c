ed18595b3d073cd8340e27ba32f049c278a803f3
Jesse <jessew7980@gmail.com>
2022-02-09 14:09:54 -0800

    Create Lab 4

---
+++ Labs/Lab 4 (Stacks)/functions.c
@@ -0,0 +1,9 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 122, Spring 2022; Lab Section 6
+* Assignment: Lab 4
+* Date: February 9, 2022
+* Description:
+*/
+
+#include "header.h"
---
+++ Labs/Lab 4 (Stacks)/main.c
@@ -0,0 +1,16 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 122, Spring 2022; Lab Section 6
+* Assignment: Lab 4
+* Date: February 9, 2022
+* Description: 
+*/
+
+#include "header.h"
+
+int main(void)
+{
+
+
+	return 0;
+}
