1a30111b062dad93492f4b9427af3f169e545f06
Jesse <jessew7980@gmail.com>
2022-02-09 19:58:08 -0800

    Update PA3

--- Programming Assignments/PA2 (Music Manager)/PA2.c
+++ Programming Assignments/PA2 (Music Manager)/PA2.c
@@ -1,8 +1,8 @@
 /*
 * Programmer: Jesse Watson
 * Class: CptS 122, Spring 2022; Lab Section 6
-* Assignment: PA2
-* Date: January 27, 2022, January 28, 2022, February 1, 2022
+* Assignment: PA2 and PA3
+* Date: January 27, 2022, January 28, 2022, February 1, 2022, February 9, 2022
 * Description: a basic digital music manager
 */
 
