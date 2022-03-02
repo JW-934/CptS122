8d768b79a9207c3367a84d5569b19c341646d61a
Jesse <jessew7980@gmail.com>
2022-03-02 09:22:28 -0800

    Create BST

---
+++ C++ Examples/BST/BST/Node.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+using std::ifstream;
+using std::ofstream;
+using std::fstream;
+
+using std::string;
