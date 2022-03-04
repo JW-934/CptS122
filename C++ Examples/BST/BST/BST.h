594e492a36387ef861e419a0e87c07d36e58befe
Jesse <jessew7980@gmail.com>
2022-03-04 11:00:41 -0800

    Update BST

--- C++ Examples/BST/BST/BST.h
+++ C++ Examples/BST/BST/BST.h
@@ -19,7 +19,35 @@ using std::string;
 template <class T>
 class BST
 {
+public:
 	BST(Node<T> *newProot = nullptr);
+
+	void insert(const T &newData);
+
 private:
 	Node<T>* mpRoot;
+
+	void insert(Node<T> *pTree, const T& newData); // This will be called recursively
 };
+
+template <class T>
+BST<T>::BST(Node<T>* newProot)
+{
+	this->mpRoot = newProot; // pointer assignment
+}
+
+template <class T>
+void BST<T>::insert(const T& newData)	// Public one
+{
+	insert(this->mpRoot, newData);
+}
+
+template <class T>
+void BST<T>::insert(Node<T> *pTree, const T& newData)	// Private one, recursive
+{
+	if (pTree == nullptr) // base case
+	{
+		//pTree = new
+		this->mpRoot = new Node<T>(newData);		// invokes constructor, needs <T>
+	}
+}
