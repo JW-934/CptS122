93da7e524bb7e545ac35d75a2d9beccb53b03743
Jesse <jessew7980@gmail.com>
2022-03-21 13:52:03 -0700

    Update PA6

--- Programming Assignments/PA6 (Morse Code BST)/Main.cpp
+++ Programming Assignments/PA6 (Morse Code BST)/Main.cpp
@@ -6,13 +6,28 @@
 * Description: A binary search tree based English to morse code converter.
 */
 
+#include <iostream>
+#include <string>
+#include <fstream>
+
 #include "BST.h"
 
 int main()
 {
+	std::string inputStr;
+	
+	std::ifstream infile();
+	infile().open("Convert.txt", std::ios::in);
+
+	if (infile().is_open()) // infile opened
+	{
+		std::getline(infile, inputStr); // in progress
 
 
-
-
+	}
+	else // infile not opened
+	{
+		std::cout << "Input file could not be opened!" << std::endl;
+	}
 	return 0;
 }
