5a6aca5b24d772f00f30efa989ae8d946905bc6a
Jesse <jessew7980@gmail.com>
2022-03-30 10:01:19 -0700

    Create Employee

---
+++ C++ Examples/Employee/Employee/Manager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "Employee.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+// Manager is a derived class, it's derived from Employee - need to complete the inheritance in class
+class Manager : public Employee // Manager is a derived class; Employee is a base class
+{
+public:
+	Manager(const string &newName = "", const double &newPayRate = 0.0, const double &newHours = 0, const bool &newSalariedEmployee = false);
+	~Manager();
+
+	// With inheritance, we should NOT have to redefine
+	// many of the functions that we find in the Employee
+	// class!
+
+
+private:
+	bool mIsSalaried; // this attribute belongs to Manager only; Employee doesn't have it.
+					  // true - salaried; false - hourly
+	// Employee mE;
+};
