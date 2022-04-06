7d2e6ec70a847f83694d1122d81e0e7cf9676c7c
Jesse <jessew7980@gmail.com>
2022-04-06 14:22:06 -0700

    Create Lab 10

---
+++ Labs/Lab 10 (Merge Sort, BST Template)/Main.cpp
@@ -0,0 +1,7 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 122, Spring 2022; Lab Section 6
+* Assignment: Lab 10
+* Date: April 6, 2022
+* Description: Implementation of merge sort and a BST template
+*/
