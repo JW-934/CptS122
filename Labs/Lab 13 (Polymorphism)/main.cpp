484e5c30aae35d33268c9eafbac83b2207bb11a0
Jesse <jessew7980@gmail.com>
2022-04-20 10:47:02 -0700

    Create Lab 13

---
+++ Labs/Lab 13 (Polymorphism)/main.cpp
@@ -0,0 +1,13 @@
+/*
+* Programmer: Jesse Watson
+* Class: CptS 122, Spring 2022; Lab Section 6
+* Assignment: Lab 13
+* Date: April 20, 2022
+* Description: Implementation of polymorphism
+*/
+
+int main()
+{
+
+	return 0;
+}
