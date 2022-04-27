ada73e1619d5c8ef608ffd5d242b138fa0747110
Jesse <jessew7980@gmail.com>
2022-04-27 10:37:12 -0700

    Update MoreCPPCode

--- C++ Examples/MoreCPPCode/main.cpp
+++ C++ Examples/MoreCPPCode/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 
 int main()
 {
@@ -8,5 +9,17 @@ int main()
 	{
 		std::cout << "element: " << i << std::endl;
 	}
+
+	std::list<int> ourList;
+	
+	ourList.push_back(10);
+	ourList.push_back(42);
+	ourList.push_front(50);
+
+	for (std::list<int>::iterator it = ourList.begin(); it != ourList.end(); ++it)
+	{
+		std::cout << "element: " << *it << std::endl;
+	}
+
 	return 0;
 }
